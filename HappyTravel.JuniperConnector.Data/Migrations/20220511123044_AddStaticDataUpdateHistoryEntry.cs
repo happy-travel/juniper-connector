7dfc8e167a03765f87cb58d10f04767c3a377e43
Айрат Молотков <airat@promo-z.ru>
2022-05-27 11:43:28 +0400

    add tables for hotel loader

---
+++ HappyTravel.JuniperConnector.Data/Migrations/20220511123044_AddStaticDataUpdateHistoryEntry.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace HappyTravel.JuniperConnector.Data.Migrations
+{
+    public partial class AddStaticDataUpdateHistoryEntry : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "StaticDataUpdateHistory",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    StartTime = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    FinishTime = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true),
+                    IsSuccess = table.Column<bool>(type: "boolean", nullable: false),
+                    Message = table.Column<string>(type: "text", nullable: true),
+                    Options = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StaticDataUpdateHistory", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "StaticDataUpdateHistory");
+        }
+    }
+}
