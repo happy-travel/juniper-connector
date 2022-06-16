40ea490e1fe603375770ca7b2588b5dec96ebe98
PKorovinPromo-Z <p.korovin@promo-z.ru>
2022-06-16 09:40:32 +0400

    DeadlineService

--- HappyTravel.JuniperConnector.Api/Services/Availabilities/Cancellations/DeadlineService.cs
+++ HappyTravel.JuniperConnector.Api/Services/Availabilities/Cancellations/DeadlineService.cs
@@ -7,7 +7,5 @@ namespace HappyTravel.JuniperConnector.Api.Services.Availabilities.Cancellations
 public class DeadlineService : IDeadlineService
 {
     public Task<Result<Deadline>> Get(string availabilityId, Guid roomContractSetId, CancellationToken cancellationToken)
-    {
-        throw new NotImplementedException();
-    }
+        => Task.FromResult(Result.Failure<Deadline>("Our current implementation and settings shows deadlines on the first two steps"));
 }
